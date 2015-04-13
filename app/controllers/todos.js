9effd2d92f2425af8842391dfa8da371c2eaec90
Tung Nguyen <tongueroo@gmail.com>
2015-04-12 17:22:33 -0700

    complete exercise

--- app/controllers/todos.js
+++ app/controllers/todos.js
@@ -1,7 +1,46 @@
 import Ember from 'ember';
  
 export default Ember.ArrayController.extend({
+    remaining: function() {
+        return this.filterBy('isCompleted', false).get('length');
+    }.property('@each.isCompleted'),
+
+    inflection: function() {
+        var remaining = this.get('remaining');
+        return (remaining === 1) ? 'item' : 'items';
+    }.property('remaining'),
+
+    hasCompleted: function() {
+        return this.get('completed') > 0;
+    }.property('completed'),
+
+    completed: function() {
+        return this.filterBy('isCompleted', true).get('length');
+    }.property('@each.isCompleted'),
+
+    allAreDone: function(key, value) {
+        console.log(key + ": " + value);
+        if (value === undefined) {
+            return this.get('length') > 0 && this.isEvery('isCompleted', true);
+        } else {
+            this.setEach('isCompleted', value);
+            this.invoke('save');
+            return value;
+        }
+    }.property('@each.isCompleted'),
+
     actions: {
+
+        clearCompleted: function() {
+            var completed = this.filterBy('isCompleted', true);
+            console.log("clearCompleted completed %o", completed);
+            console.log(this.get('title'));
+            // completed.deleteRecord();
+            // completed.save();
+            completed.invoke('deleteRecord');
+            completed.invoke('save');
+        },
+
         createTodo: function(newTitle) {
             // Create the new Todo model
             // debugger
